f232a49ccdfa91243805c8c336aca03f1f91f4dd
jaehyunkim <kimjhs854@gmail.com>
2026-01-26 21:24:55 +0100

    update js
    update js

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const gradients = [
     'linear-gradient(90deg, #f984ca, #949300)',
     'linear-gradient(90deg, #eddc4d, #796e65)',
     'linear-gradient(90deg, #9a9bd4, #7c7e7f)',
-    'linear-gradient(90deg, #d6e2f2, #70ab37)'
+    'linear-gradient(90deg, #d6e2f2, #70ab37)',
+    'linear-gradient(90deg, #9c97a8, #758448)',
+    'linear-gradient(90deg, #da291c, #686869)',
   ];
   
   let index = 0;
